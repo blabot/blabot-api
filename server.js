48f9c33145c6df740389f63c6c27bc92149783e5
Tomas Kuba <tomas.kuba.cz@gmail.com>
2015-06-21 17:07:42 +0200

    server updated

--- server.js
+++ server.js
@@ -8,15 +8,14 @@ app.get('/', function (req, res) {
 
 app.use('/v2', v2);
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   console.log(err);
   res.status(500).json({'message': err.message});
 });
 
-//var serverPort = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-//var serverIP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.PORT || 3000;        // set our port
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ipAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.listen(port, function () {
-  console.log( "Listening on: "  + port )
+app.listen(port, ipAddress, function () {
+  console.log("Listening on: " + port)
 });
